3702d4ccf8c00cf1a051e9c6b9cbe557b1a9b4a1
felipe-parra <fpr29.10@gmail.com>
2022-01-04 02:35:40 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="tooltip">
+      <span id="tooltip-text">Tooltip Text</span>
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
